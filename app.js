12cdff5541d98d5f4b24dfbe7d24ebe62ead838a
Sotos <sotiris.h.k@gmail.com>
2024-02-19 15:14:37 +0000

    completed task3 /api endpoint

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const app=express()
-const{getTopics}=require('./controllers/topics-controller')
+const{getTopics,getApiEndPoints}=require('./controllers/topics-controller')
 
 
 
 app.get('/api/topics',getTopics)
+app.get('/api',getApiEndPoints)
 
 
  
@@ -17,6 +18,7 @@ app.all("/*",(request,response,next)=>{
     response.status(404).send({msg:'Path not found'})
 })
 app.use((err,request,response,next)=>{
+
     response.status(500).send({msg:'Internal server error'})
 })
 
