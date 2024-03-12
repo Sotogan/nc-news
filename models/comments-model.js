afc6b177a69698c755b31c2fc5f1bb26be22a864
Sotos <sotiris.h.k@gmail.com>
2024-03-12 14:33:16 +0000

    added ids

--- models/comments-model.js
+++ models/comments-model.js
@@ -3,7 +3,7 @@ const db=require('../db/connection')
 
 
 exports.selectComments= (article_id)=>{
- const  articleIdValues=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','22','23','24','25','26','27','28','29','30','31','32','33','34']
+ const  articleIdValues=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34']
  if(!articleIdValues.includes(article_id) && article_id !==undefined && !isNaN(article_id)){
   return Promise.reject({status:404, msg:'id not found'})
  }   
